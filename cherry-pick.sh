3ba6b504262b4ea8dea744cefe1c7f8e60c4e9e1
Chaitanya Senapati <csenapati12@gmail.com>
2020-04-05 08:07:27 +0530

    Update cherry-pick.sh

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -1,5 +1,5 @@
 target_branch ="develop"
-if[[$1]]; then
+if [[$1]]; then
     target_branch="$1";
 fi
 commit=$(git rev-parse HEAD);
@@ -12,5 +12,5 @@ git cherry-pick "${commit}";
 git push 2> /dev/null;
 git checkout "${current_branch}";
 
-cherry_picked_commit=${git rev-parse --short ${target_branch}};
+cherry_picked_commit=${git rev-parse --short ${target_branch};
 echo "commit ${commit_short} was successfully cherry-picked to the branch ${target_branch} as commit ${cherry_picked_commit}";
